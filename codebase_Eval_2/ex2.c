edfabe3add22e6b82bbbec9f031037b29f74e43e
Fahmin Guliyev <baku.fehmin@gmail.com>
2020-04-28 11:22:10 +0400

    Some changes

--- codebase_Eval_2/ex2.c
+++ codebase_Eval_2/ex2.c
@@ -8,22 +8,25 @@ this is to determine how many threads survived and which one is which */
 
 pthread_mutex_t lock;
 
-void *function(pthread_t *th) { //a pointer function for demands of pthread_create() function
+void *function(void *arg) { //a pointer function for demands of pthread_create() function
 
     pthread_mutex_lock(&lock);      //to intervene another thread get past this line, if it does then fakeIdThread numbers will get mixed
     
-    int whichThread = ++fakeIdThread;
+    int whichThread = ++fakeIdThread; //since thread_id's can be complicated to figure out while printing, 
+    //each time a thread is entering here will be assigned a new unique number which is incremented 1 by 1
     
-    pthread_mutex_unlock(&lock);    // once mynum is set, we can unlock 
+    pthread_mutex_unlock(&lock);    // once its whichThread is set, we can unlock 
     
     struct timespec ts;
     srand(time(NULL)); // to get completely random numbers from rand() each time.If we don't bind it to time() rand() will return the same numbers 
     
     for (int i = 0 ; i < 10 ; i++ ) {
        
-        ts.tv_nsec = (((double)rand()) / RAND_MAX) * 1000000000;
+        ts.tv_nsec = (((double)rand()) / RAND_MAX) * 1000000000;  // rand returns integer therefore casting to double to successfully divide into RAND_MAX
+        // and why divide into RAND_MAX is because the largest int it can return is no more than RAND_MAX
+        // so dividing into it and multiplying by 10 to the power of 9 will give the exact amount of zeros to represent nanoseconds 
         nanosleep(&ts, NULL);
-        printf("Thread #%d <---> ",(int*)th); // must cast (pthread_t *) to (int *)
+        printf("Thread #%d <---> ",(int*)arg); // must cast to print as int
         printf("slept for %ld nanoseconds and ",ts.tv_nsec);
         printf("is on count number %d\n",i);
     }
